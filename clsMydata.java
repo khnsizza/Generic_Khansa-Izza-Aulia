5b897d42d8722e81ed24d18e6b6965e890ae0f3a
khnsizza <161116803+khnsizza@users.noreply.github.com>
2024-04-23 12:49:24 +0700

    Add files via upload

---
+++ clsMydata.java
@@ -0,0 +1,16 @@
+package generic;
+public class clsMydata <T> {
+    private T data;
+
+    public clsMydata (T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData (T data) {
+        this.data = data;
+ }
+}
---
+++ objMydata.java
@@ -0,0 +1,12 @@
+package generic;
+
+public class objMydata {
+        public static void main(String[] args) {
+            clsMydata<String> strData = new clsMydata<>("Khansa Izza Aulia");
+            System.out.println("Nama: " + strData.getData());
+
+
+        }
+    }
+    
+
